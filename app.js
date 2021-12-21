38ebcca30f13f960383525985e510afa189b1e76
CarlosGomezBurgos <gbremerasestampadas@gmail.com>
2021-12-21 14:55:39 -0300

    Update Header, Main, media-queries

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require ('express');
 const app = express();
 const path = require('path');
 app.use(express.static('public'))
-const port = 3030;
-app.listen(port, () => {
-    console.log('El servidor esta funcionando en el puerto:',port)
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('El servidor esta funcionando en el puerto:',3000)
 })
 
 app.get('/', (req, res)=>{ //analiza la ruta ./ para saber que hacer ahi.
     res.sendFile(path.resolve('./views/home.html'))
 })
+
