cdb1a5f949c082f4c48c4d9bac86c8233ca10c7d
CarlosGomezBurgos <gbremerasestampadas@gmail.com>
2022-01-03 21:38:17 -0300

    Desafio clase 15 - login,register,formularios

--- app.js
+++ app.js
@@ -10,4 +10,9 @@ app.listen(process.env.PORT || 3001, () => {
 app.get('/', (req, res)=>{ //analiza la ruta ./ para saber que hacer ahi.
     res.sendFile(path.resolve('./views/home.html'))
 })
-
+app.get('/register', (req, res)=>{ //analiza la ruta ./ para saber que hacer ahi.
+    res.sendFile(path.resolve('./views/register.html'))
+})
+app.get('/login', (req, res)=>{ //analiza la ruta ./ para saber que hacer ahi.
+    res.sendFile(path.resolve('./views/login.html'))
+})
